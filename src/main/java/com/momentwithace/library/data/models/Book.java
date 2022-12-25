c1b01b112cc286ab676721b78847168608ecad32
momemtwithace <augustineezekiel763@gmail.com>
2022-12-25 02:04:25 +0100

    Created Book Model

--- src/main/java/com/momentwithace/library/data/models/Book.java
+++ src/main/java/com/momentwithace/library/data/models/Book.java
@@ -1,4 +1,25 @@
 package com.momentwithace.library.data.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookEdition;
+    private Long bookIsbn;
+    private String bookGenre;
+
 }
